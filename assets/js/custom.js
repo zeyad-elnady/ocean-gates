bc94554a6ab7b9be4b95b263577114dddaadb13e
zeyad <zeyadelnady2@gmail.com>
2025-08-21 17:35:50 +0300

    new 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -199,31 +199,4 @@
       $(".loader-content").fadeOut(1000);
     })
 
-    // Switch Btn
-	  $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-    
 })(jQuery);
-
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-    localStorage.setItem('fixa_theme', themeName);
-    document.documentElement.className = themeName;
-}
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('fixa_theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
-// Immediately invoked function to set the theme on initial load
-(function () {
-if (localStorage.getItem('fixa_theme') === 'theme-dark') {
-    setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-    setTheme('theme-light');
-        document.getElementById('slider').checked = true;
-    }
-})();
